106cb70742033d5a2c9614c3a79eb34804b959ca
rkuchenthal <koochenthal0@gmail.co,=m>
2021-10-12 14:05:26 -0700

    Got all 3 system calls implemented and printing to cmd line

--- main.c
+++ main.c
@@ -4,11 +4,26 @@
 
 int main() {
     printf("Hello, World!\n");
-    system("ls");
-    char command[50];
 
-    strcpy( command, "dir" );
-    //system(command);
-    //system("dir");
+    //input what ever command into "command" to make it easier to
+    // make changes later on.
+    char command1[50];
+    char command2[50];
+    char command3[50];
+
+    //Displays a list of files and sub-directories in a directory.
+    printf("\n1st Command\n");
+    strcpy( command1, "dir" );
+    system(command1);
+
+    //Shows the name of or changes to a current directory.
+    printf("\n2nd Command\n");
+    strcpy( command2, "cd" );
+    system(command2);
+
+    //Displays user, group on local system
+    printf("\n3rd Command\n");
+    strcpy( command3, "whoami" );
+    system(command3);
     return 0;
 }
